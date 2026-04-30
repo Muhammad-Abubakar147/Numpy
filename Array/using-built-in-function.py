ad2b4a798be6f4ed1933dfa637d75c94b751bc7c
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-30 19:26:40 +0500

    creating array of zeros

--- Array/using-built-in-function.py
+++
@@ -1,4 +0,0 @@
-np.zeros((2, 3))   # array of zeros
-np.ones((2, 3))    # array of ones
-np.arange(0, 10, 2)  # range array
-np.linspace(0, 1, 5) # evenly spaced values
---
+++ Built-in-function/array-of-zero.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+arr = np.zeros((2, 3)) #creating zeros in array
+print(arr)
