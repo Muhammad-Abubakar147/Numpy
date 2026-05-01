ef1167196061b221564a839c3592ed7c64262eb4
Muhammad Abubakar <brandmirza702@gmail.com>
2026-05-01 23:53:18 +0500

    practice examples(conclusion of attributes of array)

---
+++ Array-Atributes/practice-example.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([[10,20,30],[40,50,60]])
+
+print("Dimensions:", arr.ndim)
+print("Shape:", arr.shape)
+print("Size:", arr.size)
+print("Datatype:", arr.dtype)
+print("Item Size:", arr.itemsize)
+print("Total Bytes:", arr.nbytes)
