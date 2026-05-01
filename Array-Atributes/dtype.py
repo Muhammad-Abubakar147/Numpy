2e702355fcce3c450d50d7b6b1e85005432f00df
Muhammad Abubakar <brandmirza702@gmail.com>
2026-05-01 23:43:59 +0500

    changing of data type

--- Array-Atributes/dtype.py
+++ Array-Atributes/dtype.py
@@ -1,3 +1,7 @@
 import numpy as np
 arr = np.array([1,2,3])
 print(arr.dtype)   # int64 (or int32 depending on system)
+
+#changing of dtype
+arr = np.array([1,2,3], dtype=float) #this will change data type
+print(arr.dtype)   # float64
