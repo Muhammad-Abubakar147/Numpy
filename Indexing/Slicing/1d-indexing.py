95c4e4cbc26fe1c15f16fe0d6aea951e23f68b51
Muhammad Abubakar <brandmirza702@gmail.com>
2026-05-03 21:50:07 +0500

    1d indexing array

--- Indexing/Slicing/1d-indexing.py
+++ Indexing/Slicing/1d-indexing.py
@@ -2,5 +2,5 @@ import numpy as np
 
 arr = np.array([10, 20, 30, 40])
 
-print(arr[0])   # First element
-print(arr[2])   # Third element
+print(arr[0])   # First element output =>10
+print(arr[2])   # Third element output =>30
